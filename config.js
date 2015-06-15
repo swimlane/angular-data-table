140ffbc5d1976202b017ce8f233032749901f043
amcdnl <amcdaniel2@gmail.com>
2015-06-15 10:52:16 -0400

    few upgrades

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ System.config({
 System.config({
   "map": {
     "angular": "npm:angular@1.4.0",
-    "babel": "npm:babel-core@5.5.6",
-    "babel-runtime": "npm:babel-runtime@5.5.6",
-    "core-js": "npm:core-js@0.8.4",
+    "babel": "npm:babel-core@5.5.8",
+    "babel-runtime": "npm:babel-runtime@5.5.8",
+    "core-js": "npm:core-js@0.9.17",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
@@ -28,8 +28,16 @@ System.config({
     "npm:babel-runtime@5.5.6": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:babel-runtime@5.5.8": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:core-js@0.8.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.17": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
