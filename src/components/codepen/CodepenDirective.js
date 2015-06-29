6d8fedb21a0ae1e75deca859ae56b7efbf92ef08
Marjan <marjan.georgiev@gmail.com>
2015-06-29 17:33:33 +0200

    clean up

--- src/components/codepen/CodepenDirective.js
+++ src/components/codepen/CodepenDirective.js
@@ -14,8 +14,6 @@
         codepenId: '='
       },
       link: function($scope, el, attributes){
-        console.log(arguments);
-
         $scope.render = function(){
           $scope.codepenUrl = $sce.trustAsResourceUrl('//codepen.io/marjan-georgiev/embed/' + $scope.codepenId +  '/?height=600&theme-id=0&default-tab=result');
         }
@@ -23,12 +21,10 @@
         $scope.render();
 
         $scope.$watch('codepenId', function(){
-          console.log($scope.codepenId);
           $scope.render();
         });
       }
     };
-
   }
 
 })();
