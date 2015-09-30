f5259d44581494e79db78bb068273fc74e29784f
Marjan <marjan.georgiev@gmail.com>
2015-09-30 15:25:59 +0200

    0.3.19

--- release/dataTable.helpers.js
+++ release/dataTable.helpers.js
@@ -1,6 +1,6 @@
 /**
  * angular-data-table - A feature-rich but lightweight ES6 AngularJS Data Table crafted for large data sets!
- * @version v0.3.18
+ * @version v0.3.19
  * @link http://swimlane.com/
  * @license 
  */
