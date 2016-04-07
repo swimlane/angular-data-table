4387800bdaef0bdcbf15db19c80082d4f8770e89
Jaap de Haan <jaap.dehaan@alpine.de>
2016-04-07 15:44:07 +0200

    [BUG] Calculate the size depending on the size of the enclosing element
    Weird things happen during resize as the div is not constrained in any way. Wrapping the datatable inside an other size-constrained parent element works with this change.
    
    Signed-off-by: Jaap de Haan <jaap.dehaan@color-of-code.de>

--- src/components/DataTableDirective.js
+++ src/components/DataTableDirective.js
@@ -79,7 +79,7 @@ export function DataTableDirective($window, $timeout, $parse){
            * Invoked on init of control or when the window is resized;
            */
           function resize() {
-            var rect = $elm[0].getBoundingClientRect();
+            var rect = $elm[0].parentNode.getBoundingClientRect();
 
             ctrl.options.internal.innerWidth = Math.floor(rect.width);
 
