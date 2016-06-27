d68eec0c8e34e4417e361cfe840462efd1120128
Marjan Georgiev <marjan.georgiev@gmail.com>
2016-06-27 20:27:34 +0200

    Ensure that column can not be resized under the minimum width

--- src/components/header/ResizableDirective.js
+++ src/components/header/ResizableDirective.js
@@ -37,7 +37,7 @@ export function ResizableDirective($document, $timeout){
 
       function mousemove(event) {
         event = event.originalEvent || event;
-        
+
         var width = parent[0].clientWidth,
             movementX = event.movementX || event.mozMovementX || (event.screenX - prevScreenX),
             newWidth = width + (movementX || 0);
@@ -52,9 +52,13 @@ export function ResizableDirective($document, $timeout){
       }
 
       function mouseup() {
-        if($scope.onResize){
-          $timeout(() => {
-            $scope.onResize({ width: parent[0].clientWidth });
+        if ($scope.onResize) {
+          $timeout(function () {
+            let width = parent[0].clientWidth;
+            if (width < $scope.minWidth){
+              width = $scope.minWidth;
+            }
+            $scope.onResize({ width: width });
           });
         }
 
