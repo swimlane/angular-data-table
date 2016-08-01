70e76c333f80eeb1a239d350ce71deeb256853f2
Austin McDaniel <amcdaniel2@gmail.com>
2016-08-01 08:49:35 -0400

    (chore): releaase

--- release/dataTable.js
+++ release/dataTable.js
@@ -1,6 +1,6 @@
 /**
  * angular-data-table - A feature-rich but lightweight ES6 AngularJS Data Table crafted for large data sets!
- * @version v0.5.2
+ * @version v0.6.0
  * @link http://swimlane.com/
  * @license 
  */
