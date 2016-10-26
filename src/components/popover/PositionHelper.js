e45641159a84484713683b0a2b82090e6c8b4d14
Jon Shaffer <jon@hyperfluidsolutions.com>
2016-10-26 09:56:09 -0400

    switch statements

--- src/components/popover/PositionHelper.js
+++ src/components/popover/PositionHelper.js
@@ -6,54 +6,56 @@ import { POSITION } from './Popover.constants';
 
 export function PositionHelper(){
   return {
-
     calculateVerticalAlignment: function(elDimensions, popoverDimensions, alignment){
-      if (alignment === POSITION.TOP){
-        return elDimensions.top;
-      }
-      if (alignment === POSITION.BOTTOM){
-        return elDimensions.top + elDimensions.height - popoverDimensions.height;
-      }
-      if (alignment === POSITION.CENTER){
-        return elDimensions.top + elDimensions.height/2 - popoverDimensions.height/2;
+      switch(alignment){
+        case POSITION.TOP:
+          return elDimensions.top;
+        case POSITION.BOTTOM:
+          return elDimensions.top + elDimensions.height - popoverDimensions.height;
+        case POSITION.CENTER:
+          return elDimensions.top + elDimensions.height/2 - popoverDimensions.height/2;
+        default:
+          return console.log('calculateVerticalAlignment issue', this);
       }
     },
 
     calculateVerticalCaret: function(elDimensions, popoverDimensions, caretDimensions, alignment){
-      if (alignment === POSITION.TOP){
-        return elDimensions.height/2 - caretDimensions.height/2 - 1;
-      }
-      if (alignment === POSITION.BOTTOM){
-        return popoverDimensions.height - elDimensions.height/2 - caretDimensions.height/2 - 1;
-      }
-      if (alignment === POSITION.CENTER){
-        return popoverDimensions.height/2 - caretDimensions.height/2 - 1;
+      switch(alignment){
+        case POSITION.TOP:
+          return elDimensions.height/2 - caretDimensions.height/2 - 1;
+        case POSITION.BOTTOM:
+          return popoverDimensions.height - elDimensions.height/2 - caretDimensions.height/2 - 1;
+        case POSITION.CENTER:
+          return popoverDimensions.height/2 - caretDimensions.height/2 - 1;
+        default:
+          return console.log('calculateVerticalCaret issue', this);
       }
     },
 
     calculateHorizontalCaret: function(elDimensions, popoverDimensions, caretDimensions, alignment){
-      if (alignment === POSITION.LEFT){
-        return elDimensions.width/2 - caretDimensions.height/2 - 1;
-      }
-      if (alignment === POSITION.RIGHT){
-        return popoverDimensions.width - elDimensions.width/2 - caretDimensions.height/2 - 1;
-      }
-      if (alignment === POSITION.CENTER){
-        return popoverDimensions.width/2 - caretDimensions.height/2 - 1;
+      switch(alignment){
+        case POSITION.LEFT:
+          return elDimensions.width/2 - caretDimensions.height/2 - 1;
+        case POSITION.RIGHT:
+          return popoverDimensions.width - elDimensions.width/2 - caretDimensions.height/2 - 1;
+        case POSITION.CENTER:
+          return popoverDimensions.width/2 - caretDimensions.height/2 - 1;
+        default:
+          return console.log('calculateHorizontalCaret issue', this);
       }
     },
 
     calculateHorizontalAlignment: function(elDimensions, popoverDimensions, alignment){
-      if (alignment === POSITION.LEFT){
-        return elDimensions.left;
-      }
-      if (alignment === POSITION.RIGHT){
-        return elDimensions.left + elDimensions.width - popoverDimensions.width;
-      }
-      if (alignment === POSITION.CENTER){
-        return elDimensions.left + elDimensions.width/2 - popoverDimensions.width/2;
+      switch(alignment){
+        case POSITION.LEFT:
+          return elDimensions.left;
+        case POSITION.RIGHT:
+          return elDimensions.left + elDimensions.width - popoverDimensions.width;
+        case POSITION.CENTER:
+          return elDimensions.left + elDimensions.width/2 - popoverDimensions.width/2;
+        default:
+          return console.log('calculateHorizontalAlignment issue', this);
       }
     }
-
   }
 };
