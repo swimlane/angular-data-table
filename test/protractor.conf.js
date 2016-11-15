decb2c99d5519c2897e561f98c65338a13564c2f
Jon Shaffer <jon@hyperfluidsolutions.com>
2016-11-15 17:12:08 -0500

    updated protractor+travis.yml for saucelabs

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,5 +1,17 @@
-exports.config = {
+const config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['**/*e2e.js']
+};
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
 }
+
+exports.config = config;
