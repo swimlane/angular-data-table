850ac80827cb76338ede75960a1c1d93b5ebfea9
Jon Shaffer <jon@hyperfluidsolutions.com>
2016-11-15 17:51:17 -0500

    updating selenium address when on travis

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -5,9 +5,11 @@ const config = {
 };
 
 if (process.env.TRAVIS) {
+  config.seleniumAddress = 'http://localhost:4445/wd/hub';
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
+    'name': 'angular-data-table e2e tests',
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
