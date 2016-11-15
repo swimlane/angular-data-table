89e59d08e5cbc6a5ab7b018920afa8354d8d7b58
Jon Shaffer <jon@hyperfluidsolutions.com>
2016-11-15 17:57:36 -0500

    progress - uses saucelabs selenium, now trying sauce user/key in since said not available

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,19 +1,21 @@
 const config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../src/**/*e2e.js']
 };
 
 if (process.env.TRAVIS) {
-  config.seleniumAddress = 'http://localhost:4445/wd/hub';
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
+    'username': process.env.SAUCE_USERNAME,
+    'accessKey': process.env.SAUCE_ACCESS_KEY,
     'name': 'angular-data-table e2e tests',
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
+} else {
+  config.seleniumAddress = 'http://localhost:4444/wd/hub';
 }
 
 exports.config = config;
