53f2b0108f8de1b7e881a76e445e82ecab5702b0
Jon Shaffer <jon@hyperfluidsolutions.com>
2016-12-11 01:32:14 -0500

    adding selenium address at :4445

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -4,12 +4,13 @@ const config = {
 };
 
 if (process.env.TRAVIS) {
-//   config.seleniumAddress = 'http://localhost:4445/wd/hub';
+  config.seleniumAddress = 'http://localhost:4445/wd/hub';
+
   config.capabilities = {
     'username': process.env.SAUCE_USERNAME,
     'accessKey': process.env.SAUCE_ACCESS_KEY,
     'name': 'angular-data-table e2e tests',
-//     'browserName': 'chrome',
+    'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
